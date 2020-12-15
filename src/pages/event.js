959fa0348fca405d0468bcffa704af3f8e440985
Harsh Rai <harshpayal3252@gmail.com>
2020-12-15 12:53:25 +0530

    Todo App

--- src/pages/event.js
+++ src/pages/event.js
@@ -19,30 +19,23 @@ function event() {
     
    
   <div className="event">
-  <h1>Our New Event</h1>
+ 
+ 
+<h1 className="coming-soon">Coming Soon</h1>
   <div className="ctflogo">
   <img src={ctf} alt=""/></div>
 
   
   <div className="ctfinfos">
-  <p>Hey Everyone
-  Team CSCODERSHUB is super excited to announce our first Jeopardy based CTF Challenge "TrollCAT CTF" in collaboration with ArishtiLive.
-  It will be a complete beginner oriented CTF challenge to level up your skills. prizes will be announced soon, stay tuned.
-  So, Team CSCODERSHUB welcomes you all to participate in this event.
-  Registration will open soon.
-  Here are Event Details:
-  Date: 23rd January, 2021
-  Time: 12:00PM IST to 6:00PM IST
-  Team type: SOLO
-  Challenge Categories:
-  <ul><li>Cryptography</li><li>OSINT</li><li>Steganography</li><li>Network Forensics</li><li>Miscellaneous</li></ul>
-
-  Don't forget to have a look at our</p>
- 
   <Row>
-    <Col xs={6}><img src={cs} alt="" className="sponser"/></Col>
-    <Col xs={6}><img src={cs2} alt="" className="sponser"/></Col>
-  </Row>
+  <Col ><img src={cs} alt="" className="sponser"/></Col>
+  <Col ><p className="collaboration">In Collaboration with</p></Col>
+  <Col ><img src={cs2} alt="" className="sponser"/></Col>
+</Row> 
+<div className="soonbg"><p className="soon">DETAILS WILL BE ANNOUNCED SOON</p></div>
+
+  
+ 
   
   </div>
 
