ed06b18b90d67ac98177b115e5d581a29343e144
Harsh Rai <harshpayal3252@gmail.com>
2021-01-02 17:50:38 +0530

    Todo App

--- src/pages/event.js
+++ src/pages/event.js
@@ -23,7 +23,7 @@ function event() {
   <div className="event">
  
  
-<h1 className="coming-soon">Coming Soon</h1>
+<h1 className="coming-soon"></h1>
   <div className="ctflogo">
   <img src={ctf} alt=""/></div>
 
@@ -35,25 +35,19 @@ function event() {
   <Col ><img src={cs2} alt="" className="sponser"/></Col>
 </Row> 
 <div className="soonbg"><p className="soon"><ul>
-<li>CTF registration starting Soon</li>
+<li>CTF registration</li>
 <li>Date: February 6th & 7th, 2021
 </li>
 <li>24 hour live CTF Challenge with security conference</li>
 
 </ul></p></div>
 <Row>
+
+<Col ><a href="https://docs.google.com/forms/d/1Vksndp_x3wAen8nTtwPFrORsKF0JTK-IKvfBxI-ymUg/viewform?edit_requested=true " ><p className="link">  <Button variant="success">Event Ambassadors link</Button></p></a></Col>
 <Col ><a href=" https://docs.google.com/forms/d/e/1FAIpQLSfDJtK76R9n6Jn5wkagcvhOxNd4ZBnUYSXsuRHq8plM8v-peA/viewform" ><p className="link">
 <Button variant="success">Conference Registration link</Button></p></a></Col>
-<Col ><a href="https://docs.google.com/forms/d/1Vksndp_x3wAen8nTtwPFrORsKF0JTK-IKvfBxI-ymUg/viewform?edit_requested=true " ><p className="link">  <Button variant="success">Event Ambassadors link</Button></p></a></Col>
-
 </Row> 
-
-
-  
- 
-  
   </div>
-
   </div>
   </Container>
   <video
